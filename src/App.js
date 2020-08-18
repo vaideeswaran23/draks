6c0a5a55f1d77949b75132cf9687d518c0fdd8f0
Vaideeswaran C <vaideeswaran.c23@gmail.com>
2020-08-18 11:51:44 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox LOL</h1>
+      <h1>Hello CodeSandbox LOL!!!</h1>
       <h2>Better to add in git. Start editing to see some magic happen!</h2>
       <p>Waste: Your browser is not supporting HTML5</p>
     </div>
