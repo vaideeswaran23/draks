833482be2b3a4f2874f29b4bb479c978c949139b
Vaideeswaran C <vaideeswaran.c23@gmail.com>
2020-08-18 11:48:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox LOL</h1>
+      <h2>Better to add in git. Start editing to see some magic happen!</h2>
+      <p>Waste: Your browser is not supporting HTML5</p>
+    </div>
+  );
+}
