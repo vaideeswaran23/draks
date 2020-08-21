c4b59ffb61f9c52bf3538407a200be27ac36de0a
ismail <ismail.fareed@zohocorp.com>
2020-08-21 19:56:21 +0530

    Custom css files added

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -11,15 +11,15 @@ function Profile(props) {
     <div className="container">
       <div className="hero is-medium is-light is-bold">
         <div className="hero-body">
-          <div className="columns">
-            <div className="column">
-              <figure class="image is-64x64">
-                <img src="https://bulma.io/images/placeholders/128x128.png" alt="Image" />
+          <div className="columns level">
+            <div className="column align-right-and-centered">
+              <figure class="image is-128x128">
+                <img className="is-rounded" src="https://bulma.io/images/placeholders/256x256.png" alt="Image" />
               </figure>
             </div>
-            <div className="column is-centered">
-              <p className="title">{profile.name}</p>
-              <p className="subtitle">{uid}</p>
+            <div className="column align-left-and-centered">
+              <p className="title align-left-and-centered">{profile.name}</p>
+              <p className="subtitle align-left-and-centered">{uid}</p>
             </div>
           </div>
         </div>
